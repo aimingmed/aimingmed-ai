bea952b0a73866a60a7d79d2be44c5951dc28b2e
leehk <leehongkai@gmail.com>
2025-02-11 16:33:58 +0800

    update config

--- app/llm-template/config.py
+++ app/llm-template/config.py
@@ -6,4 +6,5 @@ WANDB_API_KEY = "df1c4099caa8afae3fc83140689c7e799fb0cdad"
 GOOGLE_API_KEY = "AIzaSyCIRXC8q5-gKjT5TjiqLxEdKFEmOEzebNQ"
 LANGSMITH_API_KEY = "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"
 
+# Set LangSmith environment variables
 LANGCHAIN_PROJECT = 'llmops-demo'
